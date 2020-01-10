00807387058a7ae6f93816f254498d37ccc92de8
vccatherin <59666712+vccatherin@users.noreply.github.com>
2020-01-10 01:03:53 +0100

    Update soc.py

--- soc.py
+++ soc.py
@@ -114,3 +114,30 @@ class SocketAttach():
                 print("Connection established from ip :" + address[0])
             except:
                 print("Error in accepting connections")
+                
+    def manage_connections(self):
+        """
+        Handle threads and its locks.
+        """
+        while True:
+            for conn, address in zip(self.connections, self.ips):
+                try:
+                    if not self.threads[address]:
+                        threading.Thread(target=self.respond, args=(conn, address), daemon=True).start()
+                except:
+                    pass
+
+    def respond(self, conn, addr):
+        """
+        Respond to the client.
+        """
+        try:
+            self.threads[addr] = True
+            client_res = str(conn.recv(4096), "utf-8")
+            response = self.connection_api[addr].respond(client_res)
+            encoded_str = str.encode(response)
+            conn.send(encoded_str)
+            self.threads[addr] = False
+        except Exception as exps:
+            print(exps)
+
