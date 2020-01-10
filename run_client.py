3690c9298d01b0d333f91a7bf5d3c52f58022328
UdaySuhas <54909998+UdaySuhas@users.noreply.github.com>
2020-01-10 21:16:25 +0100

    Create run_client.py

---
+++ run_client.py
@@ -0,0 +1,29 @@
+"""client to connect to server"""
+import socket
+
+ip = input("Enter host ip (Press enter to keep it default) : ")
+
+if ip == "":
+    conn = socket.socket()
+    conn.connect(("127.0.0.1", 8080))
+else:
+    conn = socket.socket()
+    conn.connect((ip, 8080))
+
+def respond(inp):
+    if inp == "":
+        return
+    if INP == "quit":
+        conn.send(str.encode(INP))
+        response = conn.recv(4096)
+        print("Server -> " + str(response, "utf-8"))
+        exit()
+    conn.send(str.encode(INP))
+    response = conn.recv(4096)
+    print("Server -> " + str(response, "utf-8"))
+
+
+print("Connected to server!\n")
+while True:
+    INP = input("Input command -> ")
+    respond(INP)
